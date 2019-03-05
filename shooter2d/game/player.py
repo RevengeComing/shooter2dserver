ba83f974b68b04e5d88a15b7020bcc75e2022250
Jafar Balali Moghadam <jbalalimoghadam@cafebazaar.ir>
2019-03-05 18:03:41 +0330

    add shoot and get stones method

--- shooter2d/game/player.py
+++ shooter2d/game/player.py
@@ -1,12 +1,16 @@
+from shooter2d.game.stone import Stone
+
 
 class Player:
-    def __init__(self, x, y, name=""):
+    def __init__(self, x, y, game_map ,name=""):
+        self.game_map = game_map
         self.x = x
         self.y = y
         self.name = name
         self.speed_x = 0
         self.speed_y = 0
         self.health = 100
+        self.stone_numbers = 5
 
     def set_name(self, name):
         self.name = name
@@ -21,3 +25,10 @@ class Player:
 
     def add_hp(self, hp_amount: int):
         self.health = max(100, self.health + hp_amount)
+
+    def shoot_stein(self):
+        Stone(owner=self.name, owner_speed_x=self.speed_x, owner_speed_y=self.speed_y, game_map=self.game_map)
+        self.stone_numbers -= 1
+
+    def take_stones(self, number_of_stones):
+        self.stone_numbers += number_of_stones
--- shooter2d/game/stone.py
+++ shooter2d/game/stone.py
@@ -1,7 +1,11 @@
 
 class Stone:
-    def __init__(self, owner, owner_speed_x, owner_speed_y, stone_type="normal"):
+    def __init__(self, owner, owner_speed_x, owner_speed_y, game_map,stone_type="normal"):
         self.owner = owner
+        self.game_map = game_map
         self.type = type
         self.__speed_x = owner_speed_x * 1.5
         self.__speed_y = owner_speed_y * 1.5
+
+    def hit_wall(self):
+        pass
